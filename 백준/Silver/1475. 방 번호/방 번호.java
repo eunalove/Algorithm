cc1bb69e8fe0b21ad0db4cbb489d5ab41f0a0ffe
eunalove <88129325+eunalove@users.noreply.github.com>
2023-10-23 22:30:42 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 12792 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,3 +1,10 @@
+/*
+6과 9는 대체가능하므로 처음부터 9를 세지말고
+9대신 6을 세어준다
+0부터 8까지 존재하는 arr배열에 입력값을 세서 넣어주고
+6이 짝수면 나누기2, 홀수면 나누기2+1을 한 다음
+arr배열에서 가장 큰 값을 출력한다.
+*/
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -5,23 +12,20 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		
 		Scanner sc= new Scanner(System.in);
 		
 		String n= sc.nextLine();
-		char[] c= n.toCharArray();
-		
 		int arr[]= new int[9]; //9는 저장하지 않는다
 		
 		int ans= 0;
 		
-		for(int i=0; i<c.length; i++) {
+		for(int i=0; i<n.length(); i++) {
 			
-			if(c[i]- '0' == 9) {
+			if(n.charAt(i)- '0' == 9) {
 				arr[6] += 1;
 				
 			}else {
-			arr[c[i]- '0']+=1;
+			arr[n.charAt(i)- '0']+=1;
 			}
 		}
 		
@@ -36,13 +40,9 @@ public class Main {
 		
 		for(int i=0; i<arr.length; i++) {
 			ans= Math.max(ans, arr[i]);
-			
 		}
 		
 		System.out.print(ans);
-		
-		
-		
 		
 		
 	}
