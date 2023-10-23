cc0bac4cdc5ed1eb6199b8fb183ff5b555835715
eunalove <88129325+eunalove@users.noreply.github.com>
2023-10-24 00:09:05 +0900

    [Silver IV] Title: ATM, Time: 92 ms, Memory: 11912 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		int n= Integer.parseInt(br.readLine());
+		
+		ArrayList<Integer> list= new ArrayList<Integer>();
+		
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		
+		for(int i=0; i<n; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		Collections.sort(list);
+		
+		int sum_int= 0;
+		
+		int sum[]= new int[n];
+		sum[0]= list.get(0);
+		
+		for(int i=0; i<list.size(); i++) {
+			sum_int+=list.get(i); 
+			sum[i] = sum_int;
+		}
+		
+		int ans= 0;
+		
+		for(int x: sum) {
+			ans+= x;
+		}
+		
+		System.out.print(ans);
+		
+		
+	}
+
+}
