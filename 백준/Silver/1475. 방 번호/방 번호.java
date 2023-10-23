1a8ba0f949cdc5bbe5e435afbcefc1f6be0f5be3
eunalove <88129325+eunalove@users.noreply.github.com>
2023-10-23 22:21:28 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc= new Scanner(System.in);
+		
+		String n= sc.nextLine();
+		char[] c= n.toCharArray();
+		
+		int arr[]= new int[9]; //9는 저장하지 않는다
+		
+		int ans= 0;
+		
+		for(int i=0; i<c.length; i++) {
+			
+			if(c[i]- '0' == 9) {
+				arr[6] += 1;
+				
+			}else {
+			arr[c[i]- '0']+=1;
+			}
+		}
+		
+		if(arr[6]%2==0) {
+			arr[6] /=2;
+			
+		}else{
+			arr[6]= arr[6]/2 +1;
+			
+		};
+		
+		Arrays.sort(arr);
+		
+		System.out.print(arr[8]);
+		
+		
+		
+		
+		
+	}
+
+}
