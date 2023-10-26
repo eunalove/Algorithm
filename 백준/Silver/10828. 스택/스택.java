24bae591eded49dd108683383ccb92500a4755e0
eunalove <88129325+eunalove@users.noreply.github.com>
2023-10-27 01:57:44 +0900

    [Silver IV] Title: 스택, Time: 136 ms, Memory: 16536 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -7,41 +7,45 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		
-		int n= Integer.parseInt(br.readLine());
-		StringTokenizer st= null;
-		Stack<Integer> stack= new Stack<Integer>();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		Stack<Integer> stack = new Stack<Integer>();
+
+		StringBuilder sb = new StringBuilder();
+
 		for(int i=0; i<n; i++) {
-		
-		st= new StringTokenizer(br.readLine());
-		StringBuilder sb= new StringBuilder();
-		
-		switch(st.nextToken()) {
-		
-		case "push": int cnt= Integer.parseInt(st.nextToken());
-					stack.push(cnt);
-					break;
 			
-		case "pop":	if(stack.isEmpty()) sb.append(-1).append("\n");
-					else sb.append(stack.pop()).append("\n");
-					break;
+			st = new StringTokenizer(br.readLine());
 			
-		case "size":	sb.append(stack.size()).append("\n");
-					break;
-			
-		case "empty": if(stack.isEmpty()) sb.append(1).append("\n");
-					  else sb.append(0).append("\n"); 
-					break;
-			
-		case "top": if(stack.isEmpty()) sb.append(-1).append("\n");
-					else sb.append(stack.peek()).append("\n"); 
-					break;
-		
+			switch (st.nextToken()) {
+
+			case "push":
+				int cnt = Integer.parseInt(st.nextToken());
+				stack.push(cnt);
+				break;
+
+			case "pop":
+				sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+				break;
+
+			case "size":
+				sb.append(stack.size()).append("\n");
+				break;
+
+			case "empty":
+				sb.append(stack.isEmpty() ? 1: 0).append("\n");
+				break;
+
+			case "top":
+				sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+				break;
+
+			}
 		}
-		
 		System.out.print(sb);
-		}
 	}
+
 }
