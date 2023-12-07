e23dfb07fd39e88f843fb289d3b14fba69c26053
eunalove <88129325+eunalove@users.noreply.github.com>
2023-12-07 23:51:03 +0900

    [Silver III] Title: 이친수, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+
+		int n = Integer.parseInt(br.readLine());
+		
+		long[][] D = new long[n+1][2];
+		D[1] = new long[] {0,1};
+		
+		for(int i = 2; i < n + 1; i++) {
+			D[i][0] = D[i-1][0] + D[i-1][1];
+			D[i][1] = D[i-1][0];
+		}
+		
+		System.out.println(D[n][0] + D[n][1]);
+	}
+
+}
