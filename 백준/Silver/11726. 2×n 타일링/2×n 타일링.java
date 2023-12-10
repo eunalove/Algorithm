68c2ded537868c1b7b7e336d312f5fe5e69c108c
eunalove <88129325+eunalove@users.noreply.github.com>
2023-12-10 18:59:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 12868 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-
-    static int N;
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        N= sc.nextInt();
-
-
-        long[] dp= new long[N+2];
-
-        dp[0]= 0;
-        dp[1]= 1;
-        dp[2] =2;
-
-        for(int i=3; i<N+1; i++){
-            dp[i] = (dp[i-1]+dp[i-2])%10007;
-
-        }
-
-
-        System.out.print(dp[N]);
-    }
+	
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int n= sc.nextInt();
+		
+		int[] dp= new int[n+1];
+		
+		if(n==1) {
+			System.out.print(1);
+			
+		}else {
+			
+			//점화식 dp[i]= dp[i-1]+dp[i-2]
+			//dp[i-1]은 1X2를 하나 추가한 경우, dp[i-2]는 2X2를 추가한 경우
+			
+			dp[1]= 1;
+			dp[2]= 2;
+			
+			for(int i=3; i<=n; i++) {
+				dp[i]= dp[i-1]%10007+dp[i-2]%10007;
+				dp[i] %=10007;
+			}
+			
+			System.out.print(dp[n]);
+			
+			
+		}
+		
+	}
 
 }
