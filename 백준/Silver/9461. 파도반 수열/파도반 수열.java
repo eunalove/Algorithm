30fbedc17d849865cb3554b9d3a8c04ca47c635f
eunalove <88129325+eunalove@users.noreply.github.com>
2023-12-10 20:47:57 +0900

    [Silver III] Title: 파도반 수열, Time: 80 ms, Memory: 11460 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,30 +1,46 @@
-import java.io.*;
-import java.util.*;
+/*
+어차피 값의 범위가 100이므로 미리 dp를 만들어놓고 시작하기
+이번에도 int가 아니고 long임 잘 확인하자!!
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-
+	
 	public static void main(String[] args) throws IOException {
-
-		int T = Integer.parseInt(br.readLine());
-
-		long[] D = new long[101];
-
-		D[1] = 1; D[2] = 1; D[3] = 1;
-		D[4] = 2; D[5] = 2;
-
-		for(int i = 6; i < 101; i++) {
-			D[i] = D[i-1] + D[i-5];
+		
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int T= Integer.parseInt(br.readLine());
+		StringBuilder sb= new StringBuilder();
+		
+		long[] dp= new long[101];
+		
+		dp[1]= 1;
+		dp[2]= 1;
+		dp[3]= 1;
+		dp[4]= 2;
+		dp[5]= 2;
+		dp[6]= 3;
+		dp[7]= 4;
+		dp[8]= 5;
+		dp[9]= 7;
+		dp[10]= 9;
+		
+		for(int i=11; i<=100; i++) {
+			dp[i]= dp[i-1]+dp[i-5];
 		}
 		
-		while(T-- > 0) {
-			int n = Integer.parseInt(br.readLine());
+		for(int t=0; t<T; t++) {
 			
-			System.out.println(D[n]);
+			int n= Integer.parseInt(br.readLine());
+			sb.append(dp[n]).append("\n");
+			
+			
 		}
+		
+		System.out.print(sb);
+		
 		
 	}
 
