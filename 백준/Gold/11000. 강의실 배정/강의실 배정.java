1176b15bcd1db7f8ce6321f8ecd2b84e4dd2d753
eunalove <88129325+eunalove@users.noreply.github.com>
2023-12-14 23:50:17 +0900

    [Gold V] Title: 강의실 배정, Time: 844 ms, Memory: 92532 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static boolean[] chk;
+	static int n, end;
+	static TreeSet<int[]> tm = new TreeSet<>((a,b) -> {
+		if(a[0] == b[0]) return a[2] - b[2];
+		return a[0] - b[0];
+	});
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		n = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+
+			tm.add(new int[] {s,e,i});
+		}
+	
+		
+		int res = 0, etime = 0;
+		while(!tm.isEmpty()) {
+			
+			int[] c = tm.pollFirst();
+			res++;
+			etime = c[1];
+			
+			while(!tm.isEmpty()) {
+				int[] p = tm.ceiling(new int[] {etime,0,0});
+				if(p == null)
+					break;
+				else {
+					etime = p[1];
+					tm.remove(p);
+				}
+			}
+		}
+		
+		System.out.println(res);
+		
+		
+
+	}
+	
+
+}
