eca6cb6aba8a799bce3dee8c816e423516bdee01
eunalove <88129325+eunalove@users.noreply.github.com>
2023-12-27 00:03:27 +0900

    [Gold V] Title: 수 고르기, Time: 392 ms, Memory: 28136 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,3 +1,9 @@
+/*
+이분탐색을 이용함
+lower를 이용해서 특정 값보다 같거나 직후로 큰 값을 구함
+for문으로 i를 모두 돌면서 lower로 arr[mid]- arr[i]가 특정 값보다 작은지 체크함
+O(10만)*O(log10만)하면 대략 160만이 나온다.
+*/
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -32,8 +38,6 @@ public class Main {
 
 	 static void lowerBound() {
 		 
-		 //2 3 3 3 5, 4를 입력하면 5가 나와야함
-		 
 		 int low= 0;
 		 int high= arr.length;
 		 int mid= 0;
@@ -50,11 +54,13 @@ public class Main {
 			 if(arr[mid]- arr[i] < m) low= mid+1;
 			 else high= mid;
 		 }
-		 
-		 if(high == n) continue;
+             
+         //하지 않으면 런타임 오류남
+         if(high== n) continue;
 		 
 		 ans= Math.min(ans, arr[high]- arr[i]);
 		 
+         //이미 m값이 나왔으므로 더이상 탐색할 필요없음
 		 if(ans == m) break;
 		 
 	}
