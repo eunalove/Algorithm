4def4cf8904450365e7bf82d90fd511c659bf0cc
eunalove <88129325+eunalove@users.noreply.github.com>
2023-12-27 02:40:46 +0900

    [Gold V] Title: 수 고르기, Time: 292 ms, Memory: 27428 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,9 +1,3 @@
-/*
-이분탐색을 이용함
-lower를 이용해서 특정 값보다 같거나 직후로 큰 값을 구함
-for문으로 i를 모두 돌면서 lower로 arr[mid]- arr[i]가 특정 값보다 작은지 체크함
-O(10만)*O(log10만)하면 대략 160만이 나온다.
-*/
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,18 +6,16 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	static int[] arr;
-	static int n, m, ans= Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st= new StringTokenizer(br.readLine());
 		
-		n= Integer.parseInt(st.nextToken());
-		m= Integer.parseInt(st.nextToken());
+		int n= Integer.parseInt(st.nextToken());
+		int m= Integer.parseInt(st.nextToken());
+		int ans= Integer.MAX_VALUE;
 		
-		arr= new int[n];
+		int[] arr= new int[n];
 		
 		for(int i=0; i<n; i++) {
 			arr[i]= Integer.parseInt(br.readLine());
@@ -31,37 +23,31 @@ public class Main {
 		
 		Arrays.sort(arr);
 		
-		lowerBound();
+		int start=0; int end= 0;
+		
+		while(end < n) {
+		
+			int min= 0;
+			
+			if(arr[end]- arr[start] == m) {
+				ans= m;
+				break;
+			}
+			
+			if(arr[end]- arr[start] < m)
+			{
+				end+=1;
+				continue;
+			}
+			
+			
+			min= arr[end]- arr[start];
+			ans= Math.min(ans, min); //0	
+			start+=1;
+		
+		}
 		
 		System.out.print(ans);
 	}
-
-	 static void lowerBound() {
-		 
-		 int low= 0;
-		 int high= arr.length;
-		 int mid= 0;
-		 
-		 for(int i=0; i<n; i++) {
-			 
-			 low= i;
-			 high= n;
-			 
-		 while(low < high) {
-			 
-			 mid= (low+high)/2;
-			 
-			 if(arr[mid]- arr[i] < m) low= mid+1;
-			 else high= mid;
-		 }
-             
-         //하지 않으면 런타임 오류남
-         if(high== n) continue;
-		 
-		 ans= Math.min(ans, arr[high]- arr[i]);
-		 
-		 
-	}
-	 }
-
+	
 }
