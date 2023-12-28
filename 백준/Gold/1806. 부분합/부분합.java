9a3f576dc5ec0e72e16328c885882a07d509e5cc
eunalove <88129325+eunalove@users.noreply.github.com>
2023-12-28 19:54:41 +0900

    [Gold IV] Title: 부분합, Time: 208 ms, Memory: 21924 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,40 +1,52 @@
-import java.io.*;
-import java.util.*;
+/*
+투포인터의 가장 중요한 점은 start 이후에 end가 존재해야만 한다는 점이다.
+start가 증가하면 end도 항상 증가해야한다.
+그리고 인덱스 범위 항상 주의!!
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-
+public class Main {
+	
 	public static void main(String[] args) throws IOException {
-
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int s = Integer.parseInt(st.nextToken());
-
-		int[] nums = new int[n+1];
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++)
-			nums[i] = Integer.parseInt(st.nextToken());
-
-		int min = Integer.MAX_VALUE;
-
-		int eidx = 0, sum = nums[0];
-		boolean flag = true;
 		
-		for(int sidx = 0; sidx < n; sidx++) {
-			while(eidx < n && sum < s) {
-				sum += nums[++eidx];
-			}
-			if(eidx == n)
-				break;
-			min = min > eidx - sidx + 1? eidx - sidx + 1 : min;
-			sum -= nums[sidx];
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		
+		int n= Integer.parseInt(st.nextToken());
+		int s= Integer.parseInt(st.nextToken());
+		
+		int[] arr= new int[n+1];
+		
+		st= new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i]= Integer.parseInt(st.nextToken());
 		}
 		
-		if(min == Integer.MAX_VALUE) min = 0;
-		System.out.println(min);
+		
+		int end= 0;
+		int sum= arr[0];
+		int ans= Integer.MAX_VALUE;
+		
+		
+		for(int start=0; start<n; start++) {
+			while(end < n && sum < s) {
+				sum += arr[++end];
+			}
+			
+			if(end == n) break;
+
+			ans= ans> end- start +1 ? end- start+1: ans;
+			sum-= arr[start];
+				
+			}
+		
+		if(ans== Integer.MAX_VALUE) System.out.print(0);
+		else System.out.print(ans);
+		
 		
 	}
 
