ccefd10d993eec363cd9e559cd8d900e00039dd4
eunalove <88129325+eunalove@users.noreply.github.com>
2024-01-01 00:33:09 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 292 ms, Memory: 37136 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,36 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        int [] numbers = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int [] count = new int[100001];
-        int start = 0, end = 0;
-        int answer = Integer.MIN_VALUE;
-
-        while (end < N) {
-            while (end < N && count[numbers[end]] + 1 <= K) {
-                count[numbers[end++]]++;
-            }
-            int len = end - start;
-            answer = Math.max(answer, len);
-
-            count[numbers[start++]]--;
-        }
-
-        System.out.print(answer);
-    }
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] count = new int[100001];		
+		int ans = 0;
+		int start = 0;
+		
+		for (int end = 0; end < n; end++) {
+			count[arr[end]]++;
+			
+			while (count[arr[end]] > k) {
+				count[arr[start]]--;
+				start++;
+			}
+			
+			ans = Math.max(ans, end - start + 1);
+		}
+		
+		System.out.print(ans);
+	}
 }
