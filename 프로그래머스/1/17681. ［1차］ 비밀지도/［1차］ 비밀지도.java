850f9a1a58ee3d39dc96575cbba932a6f86f06f4
eunalove <88129325+eunalove@users.noreply.github.com>
2024-01-21 23:55:36 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,29 +2,47 @@ import java.util.*;
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
         
-        char[][] map = new char[n][n];
+        //n*n 배열을 만든다
+        char[][] map= new char[n][n];
         
-        for(int i = 0; i < n; i++) {
+        //arr1, arr2를 이진수로 만들어서 배열에 넣는다
+        String[] two1= new String[n];
+        String[] two2= new String[n];
+        
+        for(int i=0; i<n; i++){
+            two1[i]= String.format("%0"+n+"d", Integer.parseInt(Integer.toBinaryString(arr1[i])));
+            two2[i]= String.format("%0"+n+"d", Integer.parseInt(Integer.toBinaryString(arr2[i])));
+        }
+        
+        
+        //각 배열을 돌면서 XOR연산을 해서 n*n배열에 넣는다
+        //two1= 01001, 10100, 11100, 10010, 1011
+        //two2= 10100, 1, 10101, 10001, 11100
+        for(int i=0; i<n; i++){ //하나씩 비교한다
             
-            for(int j = n-1; j >= 0; j--) {
-                int a1 = arr1[i] % 2;
-                int a2 = arr2[i] % 2;
+            for(int j=0; j<n; j++){ //char끼리 하나씩 비교
                 
-                if(a1 + a2 == 0) {
-                    map[i][j] = ' ';
-                } else {
-                    map[i][j] = '#';
+                if(two1[i].charAt(j)=='1' || two2[i].charAt(j)=='1'){
+                    //둘 다 1이면 1
+                    map[i][j]= '#';
+                    
+                }else{
+                    map[i][j]= ' ';
+                    
                 }
-                arr1[i] /= 2;
-                arr2[i] /= 2;
             }
         }
         
-        for(int i = 0; i < n; i++)
-            answer[i] = String.valueOf(map[i]);
+        String[] answer = new String[n];
         
+        //n*n배열에서 0이면 공백, 1이면 #을 answer에 넣는다
+        for(int i=0; i<n; i++){
+            
+           answer[i]= String.valueOf(map[i]);
+            
+        }
+      
         return answer;
     }
 }
