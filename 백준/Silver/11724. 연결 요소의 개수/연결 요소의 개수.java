144e515c68f7f543888b45dbde939a22a937cbd6
eunalove <88129325+eunalove@users.noreply.github.com>
2024-01-29 00:07:49 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 580 ms, Memory: 144484 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,57 +1,68 @@
+//항상 dfs로 그래프를 풀다가 bfs로 푸니까 신선했다.
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-
-	public static void main(String[] args) throws IOException {
-
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-
-		boolean[] vis = new boolean[n+1];
+	
+	static ArrayList<Integer>[] list;
+	static boolean[] visited;
+	
+	public static void main(String[] args)throws IOException {
 		
-		ArrayList<Integer>[] graph = new ArrayList[n+1];
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
 		
-		for(int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			if(graph[a] == null)
-				graph[a] = new ArrayList<>();
-			if(graph[b] == null)
-				graph[b] = new ArrayList<>();
-			graph[a].add(b);
-			graph[b].add(a);
+		int n= Integer.parseInt(st.nextToken());
+		int m= Integer.parseInt(st.nextToken());
+		
+		list= new ArrayList[n+1];
+		
+		for(int i=1; i<=n; i++) {
+			list[i]= new ArrayList<Integer>();
 		}
 		
-		Queue<Integer> q = new ArrayDeque<>();
-		int cc = 0;
-		
-		for(int i = 1; i < n+1; i++) {
-			if(vis[i]) continue;
+		while(m-->0) { //그래프에 값 넣어줌
+			st= new StringTokenizer(br.readLine());
 			
-			q.offer(i);
-			vis[i] = true;
-			cc++;
-			while(!q.isEmpty()) {
-				int v = q.poll();
-				
-				if(graph[v] != null)
-					for(int nv : graph[v]) {
-						if(vis[nv]) continue;
-						q.offer(nv);
-						vis[nv] = true;
-					}
-			}
+			int a= Integer.parseInt(st.nextToken());
+			int b= Integer.parseInt(st.nextToken());
+			
+			list[a].add(b);
+			list[b].add(a);
+			
 		}
 		
-		System.out.println(cc);
+		Queue<Integer> q= new ArrayDeque<Integer>();
+		visited= new boolean[n+1];
+		int ans= 0;
+		
+		for(int i=1; i<=n; i++) {
+		
+		if(visited[i]) continue;
+		
+		q.offer(i);
+		visited[i]= true;
+		
+		ans++;
+		
+		while(!q.isEmpty()) {
+			
+			int v= q.poll();
+			
+			for(int x: list[v]) {
+				
+				if(visited[x]) continue;
+				q.offer(x);
+				visited[x]= true;
+			}
+			
+		}
+		
+		
+		}
+		
+		System.out.print(ans);
 
 	}
 
