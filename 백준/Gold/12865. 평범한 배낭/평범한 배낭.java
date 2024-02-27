0f5c2cdc1bc26bfcdb32530eb75e6890ff63b31e
eunalove <88129325+eunalove@users.noreply.github.com>
2024-02-28 00:22:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 51232 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,40 +1,44 @@
-import java.io.*;
-import java.util.*;
+//https://fbtmdwhd33.tistory.com/60 참조
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int t = Integer.parseInt(st.nextToken());
+    public static void main(String[] args)throws Exception {
 
-		int[][] info = new int[n + 1][2];
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
 
-		for (int i = 1; i < n + 1; i++) {
-			st = new StringTokenizer(br.readLine());
-			info[i][0] = Integer.parseInt(st.nextToken());
-			info[i][1] = Integer.parseInt(st.nextToken());
-		}
+        int n= Integer.parseInt(st.nextToken());
+        int k= Integer.parseInt(st.nextToken());
 
-		int[][] dp = new int[n + 1][t + 1];
-		for (int i = 1; i < t + 1; i++) {
+        int[] w= new int[n+1];
+        int[] v= new int[n+1];
 
-			for (int j = 1; j < n + 1; j++) {
-				dp[j][i] = dp[j - 1][i] > dp[j][i - 1] ? dp[j - 1][i] : dp[j][i - 1];
+        for(int i=1; i<=n; i++){
+            st= new StringTokenizer(br.readLine());
+            w[i]= Integer.parseInt(st.nextToken());
+            v[i]= Integer.parseInt(st.nextToken());
+        }
 
-				if (i < info[j][0])
-					continue;
+        int[][] dp= new int[n+1][k+1];
 
-				dp[j][i] = dp[j][i] > dp[j - 1][i - info[j][0]] + info[j][1] ? dp[j][i]
-						: dp[j - 1][i - info[j][0]] + info[j][1];
-			}
-		}
+        for(int i=1; i<= n; i++){//아이템 갯수만큼 돌린다
+            for(int j= 1; j<=k; j++){//가질 수 있는 value만큼 돌린다
 
-		System.out.println(dp[n][t]);
+                dp[i][j]= dp[i-1][j]; // 기본적으로 이전 아이템의 가치를 저장한다.
+                if(j >= w[i])  // 무게에서 자신의 무게를 뺐을 때 남는 무게가 존재하면,
+                    // 이전 아이템에서 구한 가치와 남은 무게의 가치 + 자신의 가치 중 큰 값을 취한다.
+                    dp[i][j]= Math.max(dp[i-1][j], dp[i-1][j-w[i]]+ v[i]);
 
-	}
+            }
+
+        }
+
+        System.out.print(dp[n][k]);
+
+    }
 }
