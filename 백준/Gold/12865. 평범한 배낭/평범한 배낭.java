261f65a3fb232492960a365f581b86dde9f82e3a
eunalove <88129325+eunalove@users.noreply.github.com>
2024-03-12 09:13:54 +0900

    [Gold V] Title: 평범한 배낭, Time: 112 ms, Memory: 12112 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,44 +1,34 @@
-//https://fbtmdwhd33.tistory.com/60 참조
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-
-
-    public static void main(String[] args)throws Exception {
-
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-
-        int n= Integer.parseInt(st.nextToken());
-        int k= Integer.parseInt(st.nextToken());
-
-        int[] w= new int[n+1];
-        int[] v= new int[n+1];
-
-        for(int i=1; i<=n; i++){
-            st= new StringTokenizer(br.readLine());
-            w[i]= Integer.parseInt(st.nextToken());
-            v[i]= Integer.parseInt(st.nextToken());
-        }
-
-        int[][] dp= new int[n+1][k+1];
-
-        for(int i=1; i<= n; i++){//아이템 갯수만큼 돌린다
-            for(int j= 1; j<=k; j++){//가질 수 있는 value만큼 돌린다
-
-                dp[i][j]= dp[i-1][j]; // 기본적으로 이전 아이템의 가치를 저장한다.
-                if(j >= w[i])  // 무게에서 자신의 무게를 뺐을 때 남는 무게가 존재하면,
-                    // 이전 아이템에서 구한 가치와 남은 무게의 가치 + 자신의 가치 중 큰 값을 취한다.
-                    dp[i][j]= Math.max(dp[i-1][j], dp[i-1][j-w[i]]+ v[i]);
-
-            }
-
-        }
-
-        System.out.print(dp[n][k]);
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+ 
+		int[] W = new int[N + 1]; // 무게
+		int[] V = new int[N + 1]; // 가치
+		int[] dp = new int[K + 1];
+ 
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			W[i] = Integer.parseInt(st.nextToken());
+			V[i] = Integer.parseInt(st.nextToken());
+		}
+ 
+		for (int i = 1; i <= N; i++) {
+			
+			// K부터 탐색하여 담을 수 있는 무게 한계치가 넘지 않을 때까지 반복 
+			for (int j = K; j - W[i] >= 0; j--) {
+				dp[j] = Math.max(dp[j], dp[j - W[i]] + V[i]);
+			}
+		}
+		System.out.println(dp[K]);
+	}
 }
