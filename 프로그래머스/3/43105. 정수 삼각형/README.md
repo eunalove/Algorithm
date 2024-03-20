4e78f23f74d79118f0f56028f91fdd006479abf5
eunalove <88129325+eunalove@users.noreply.github.com>
2024-03-21 00:13:45 +0900

    [level 3] Title: 정수 삼각형, Time: 9.42 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
 
 ### 성능 요약
 
-메모리: 61.9 MB, 시간: 9.19 ms
+메모리: 61.6 MB, 시간: 9.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 19일 22:08:29
+2024년 03월 21일 00:13:43
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -3,39 +3,35 @@ import java.io.*;
 
 class Solution {
     public int solution(int[][] tri) {
-        if(tri == null || tri.length == 0) return 0;
-
-        // 동적 프로그래밍 배열 초기화
-        long[][] dp = new long[tri.length][];
-        for (int i = 0; i < tri.length; i++) {
-            dp[i] = new long[tri[i].length];
-        }
-
-        // 시작 위치 초기화
-        dp[0][0] = tri[0][0];
-
-        // 삼각형 내려가면서 최댓값 계산
-        for (int i = 1; i < tri.length; i++) {
-            for (int j = 0; j < tri[i].length; j++) {
-                if (j == 0) {
-                    // 왼쪽 끝일 때
-                    dp[i][j] = dp[i-1][j] + tri[i][j];
-                } else if (j == tri[i].length - 1) {
-                    // 오른쪽 끝일 때
-                    dp[i][j] = dp[i-1][j-1] + tri[i][j];
-                } else {
-                    // 중간 위치일 때
-                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + tri[i][j];
-                }
+        int ans= 0;
+        int n= tri.length;
+        
+        //1. dp배열 생성
+        int[][] dp= new int[n][n];
+        
+        
+        //2. 초기값 세팅
+        dp[0][0]= tri[0][0];
+        
+        //3. 왼쪽을 일단 세팅하기
+        for(int i=1; i<n; i++)
+            dp[i][0]= dp[i-1][0]+ tri[i][0];
+        
+        
+        //3. 왼쪽 위나 오른쪽 위 중에서 더 값이 큰것을 저장함
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=i; j++){
+                dp[i][j]= Math.max(dp[i-1][j],dp[i-1][j-1])+ tri[i][j];
             }
         }
-
-        // 마지막 행에서 최대값 찾기
-        long answer = 0;
-        for (int i = 0; i < dp[tri.length-1].length; i++) {
-            answer = Math.max(answer, dp[tri.length-1][i]);
-        }
-
-        return (int)answer;
+        
+        
+        //4. 마지막 줄을 돌면서 가장 큰 값을 찾음
+        for(int x: dp[n-1])
+            ans = Math.max(ans,x);
+        
+        
+        
+        return ans;
     }
 }
