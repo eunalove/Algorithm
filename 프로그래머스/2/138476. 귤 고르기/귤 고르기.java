9f3387a180e452540fd6a819e8f4a4f7db5ffadc
eunalove <88129325+eunalove@users.noreply.github.com>
2024-03-31 19:56:50 +0900

    [level 2] Title: 귤 고르기, Time: 80.01 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,42 +1,33 @@
 import java.util.*;
-import java.util.Map.*;
 
 class Solution {
-    public int solution(int k, int[] tangerine) {
-        //map으로 귤 크기를 세고 그 중에서 갯수가 작은 순서대로 없애버리기
+    public int solution(int k, int[] tan) {
         
-        HashMap<Integer, Integer> map= new HashMap<>();
+        //HashMap key는 귤의 크기, value는 뒷자리는 귤의 갯수
+        int ans= 0;
         
-        for(int i=0; i<tangerine.length; i++){
-            int element= tangerine[i];
-            map.put(element, map.getOrDefault(element, 0)+1);
+        //map에 담기
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i: tan)
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        
+        
+        //key 정렬
+        List<Integer> keyList= new ArrayList<Integer>(map.keySet());
+        
+        keyList.sort((k1, k2) -> map.get(k2) - map.get(k1));
+        
+        //앞에서부터 k개 뽑기
+        for(int key: keyList){
+            k-= map.get(key);
+            ans++;
+            if(k <= 0) break;
         }
-        
-        int[][] arr= new int[map.size()][2];
-        
-        int cnt= 0;
-        //value값 크기가 큰 순서대로 정렬하기
-        for(Entry<Integer, Integer> entry: map.entrySet()){
             
-            arr[cnt][0] = entry.getKey();
-            arr[cnt][1] = entry.getValue();
-            cnt+=1;
-        }
-        
-        Arrays.sort(arr, (a1, a2)->{
-            return -(a1[1]- a2[1]);
-        });
-            
-        int sum= 0;
-        int answer = 0;
-        
-        for(int i=0; i<arr.length; i++){
-            sum += arr[i][1];
-            answer+=1;
-            if(sum >= k) break;
-        }    
+                    
+        return ans;
         
         
-        return answer;
     }
 }
