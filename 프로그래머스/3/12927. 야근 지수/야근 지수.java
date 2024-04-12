142fccf4a8aa14a20c7047b1aebf423708b7577c
eunalove <88129325+eunalove@users.noreply.github.com>
2024-04-12 22:41:44 +0900

    [level 3] Title: 야근 지수, Time: 118.55 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -4,8 +4,6 @@ class Solution {
     public long solution(int n, int[] works) {
         long ans = 0;
         
-        //그리디가 과연 맞을까?
-        
         //최대값을 계속 유지하기 위해 pq생성
         PriorityQueue<Integer> pq= new PriorityQueue<Integer>(Collections.reverseOrder());
         int len= works.length;
@@ -16,12 +14,12 @@ class Solution {
         //n만큼 돌면서 works에서 가장 큰 값에서 1씩 뺀다
         for(int i=0; i<n; i++){
             int tmp= pq.poll();
-            if(tmp > 0)
+            if(tmp > 0) //만약 마이너스라면 마이너스*마이너스로 예상치 못한 값이 나올 수 있음
             pq.offer(tmp-1);
             else pq.offer(0);
         }
         
-        while(!pq.isEmpty()){
+        while(!pq.isEmpty()){//남은 일의 작업량으로 야근지수를 구한다
             int tmp= pq.poll();
             ans+= tmp*tmp;
         }
