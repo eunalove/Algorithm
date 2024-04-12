263f602ae685eb9cc0825136732d41b9a8a3f32f
eunalove <88129325+eunalove@users.noreply.github.com>
2024-04-12 22:07:22 +0900

    [level 3] Title: 섬 연결하기, Time: 0.71 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -5,74 +5,87 @@ class Solution {
     
     static int[] p;
     
-    static class Edge implements Comparable<Edge> {
+    static class Edge{
         int from;
         int to;
         int w;
         
         public Edge(int from, int to, int w){
-            this.from = from;
-            this.to = to;
-            this.w = w;
+            this.from= from;
+            this.to= to;
+            this.w= w;
         }
-
-        @Override
-        public int compareTo(Edge o) {
-            return this.w - o.w;
-        }
+        
     }
     
     public int solution(int n, int[][] costs) {
-        int ans = 0;
-        // MST를 푸는 방법
-
-        int v = costs.length;
+        int v= costs.length;
         
-        Edge[] edges = new Edge[v]; // costs.length로 수정
+        Edge[] edge= new Edge[v];
         
-        for(int i = 0; i < v; i++){ // v로 변경
-            
+        
+        //cost저장하기
+        for(int i=0; i<v; i++){
             int from = costs[i][0];
             int to = costs[i][1];
             int w = costs[i][2];
             
-            edges[i] = new Edge(from, to, w);
+            edge[i]= new Edge(from, to, w);
         }
+        
+        //정렬하기 다익스트라는 시작점이 정해져있지만 MST는 직접 찾아야함
+        Arrays.sort(edge, (e1, e2)->{
+            return e1.w- e2.w;
+        });
+        
+        int ans=0;
+        
+        //유니온 파인드에 쓰일 p[] 초기값
+        p= new int[n];
+        
+        for(int i=0; i<n; i++)
+            p[i]= i;
+        
+        int cnt= 1;
+        //같은 부모인지 찾기
+        for(int i=0; i<edge.length; i++){
             
-        // 유니온-파운드에서 사용될 배열 선언
-        p = new int[n]; // n으로 수정
-        
-        for(int i = 0; i < n; i++) // n으로 수정
-            p[i] = i;
-        
-        Arrays.sort(edges); // Comparator 대신 Comparable 인터페이스 구현
-        
-        // 같은 부모를 가지지 않으면 union 하고 비용을 더한다
-        for(int i = 0; i < v; i++){ // v로 변경
-            if(union(edges[i].from, edges[i].to)){
-                ans += edges[i].w;
+            //같으면 ans에 w더하기
+            if(!union(edge[i].from, edge[i].to)) {
+                cnt++;
+                ans+= edge[i].w;
+                if(cnt == n) return ans;
             }
+            
         }
         
         return ans;
-    }
-    
-    static boolean union(int a, int b){
         
-        a = find(a);
-        b = find(b);
-        
-        if(a == b) return false; // 이미 같은 집합에 속해 있는 경우
-        
-        p[b] = a; // b의 부모를 a로 설정
-        return true;
         
     }
     
-    static int find(int a){
+    static boolean union(int a, int b){//두 매개변수의 부모가 같은지 다른지 확인하는 함수
+        
+        a= find(a);//a의 부모를 찾는다
+        b= find(b);//b의 부모를 찾는다
+        
+        if(a==b) return true;//같으면 true를 리턴한다
+        
+        //아니면 b의 부모를 a로 옮긴다
+        p[b] =a;
+        
+        //false를 리턴한다
+        return false;
+    }
+    
+    
+    static int find(int a){// a의 부모를 알려주는 함수
         
         if(p[a] == a) return a;
         
-        return p[a] = find(p[a]);
+        return p[a]= find(p[a]);
+        
     }
+    
+    
 }
