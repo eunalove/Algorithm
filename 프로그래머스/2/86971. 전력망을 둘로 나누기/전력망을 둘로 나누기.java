56c5988fd22d1b8e5a3cf49c851df1198d169fd1
eunalove <88129325+eunalove@users.noreply.github.com>
2024-04-26 10:55:50 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.45 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,54 +3,56 @@ import java.util.*;
 class Solution {
     
     static ArrayList<Integer>[] graph;
-    static int ans = Integer.MAX_VALUE;
+    static int ans= Integer.MAX_VALUE;
     static int[][] wires2;
     
     public int solution(int n, int[][] wires) {
-        wires2 = new int[wires.length][2]; // wires2 초기화
+        wires2= new int[wires.length][2];
         
-        for(int i = 0; i < wires.length; i++) {
-            wires2[i][0] = wires[i][0];
-            wires2[i][1] = wires[i][1];
+        for(int i=0; i<wires.length; i++){
+            wires2[i][0]= wires[i][0];
+            wires2[i][1]= wires[i][1];
         }
         
-        for(int i = 0; i < wires.length; i++) { // 연결을 하나씩 제외하면서 확인
-            init(i, n); // i번째 연결을 제외하고 초기화
-            bfs(1, n); // 항상 1번 노드부터 탐색 시작
+        for(int i=0; i< wires.length; i++){
+            init(i, n);
+            bfs(wires[0][0], n);
         }
         
         return ans;
     }
     
-    static void init(int exclude, int n) {
-        graph = new ArrayList[n+1];
+    static void init(int cur, int n){
+        graph= new ArrayList[n+1];
         
-        for(int i = 1; i <= n; i++)
-            graph[i] = new ArrayList<Integer>();
+        for(int i=1; i<=n; i++)
+            graph[i]= new ArrayList<Integer>();
         
-        for(int i = 0; i < wires2.length; i++) {
-            if(i == exclude) continue; // exclude 인덱스의 연결을 제외
+         for(int i=0; i<wires2.length; i++){
+            if(i == cur) continue;
             graph[wires2[i][0]].add(wires2[i][1]);
             graph[wires2[i][1]].add(wires2[i][0]);
+            
         }
+        
     }
         
-    static void bfs(int start, int n) {
+    static void bfs(int cur, int n){
         
-        Queue<Integer> q = new ArrayDeque<Integer>();
-        q.add(start);
+        Queue<Integer> q= new ArrayDeque<Integer>();
+        q.add(cur);
         
-        boolean[] vis = new boolean[n+1];
-        vis[start] = true; // 시작 노드를 방문 처리
+        boolean[] vis= new boolean[n+1];
+        vis[cur]= true;
         
-        while(!q.isEmpty()) {
+        while(!q.isEmpty()){
             
-            int tmp = q.poll();
+            int tmp= q.poll();
             
-            for(int x : graph[tmp]) {
+            for(int x: graph[tmp]){
                 
-                if(!vis[x]) {
-                    vis[x] = true;
+                if(!vis[x]){
+                    vis[x]= true;
                     q.add(x);
                 }
                 
@@ -64,6 +66,7 @@ class Solution {
         int two = n - one; // 전체 노드 수에서 one을 빼서 다른 그룹의 크기를 구함
         
         ans = Math.min(ans, Math.abs(one-two));
+        
         
     }
 }
