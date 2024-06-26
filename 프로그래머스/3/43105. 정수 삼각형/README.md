73168ae146afb6bb9e6fcbc0392f599d77f1ba32
eunalove <88129325+eunalove@users.noreply.github.com>
2024-06-26 17:56:20 +0900

    [level 3] Title: 정수 삼각형, Time: 12.56 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 61.6 MB, 시간: 9.42 ms
+메모리: 58.6 MB, 시간: 12.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 21일 00:13:43
+2024년 06월 26일 17:56:16
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,36 +2,27 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
+    
     public int solution(int[][] tri) {
-        int ans= 0;
         int n= tri.length;
+        int ans= Integer.MIN_VALUE;
         
-        //1. dp배열 생성
         int[][] dp= new int[n][n];
         
-        
-        //2. 초기값 세팅
         dp[0][0]= tri[0][0];
         
-        //3. 왼쪽을 일단 세팅하기
-        for(int i=1; i<n; i++)
-            dp[i][0]= dp[i-1][0]+ tri[i][0];
-        
-        
-        //3. 왼쪽 위나 오른쪽 위 중에서 더 값이 큰것을 저장함
-        for(int i=1; i<n; i++){
-            for(int j=1; j<=i; j++){
-                dp[i][j]= Math.max(dp[i-1][j],dp[i-1][j-1])+ tri[i][j];
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<=i; j++){
+                
+            dp[i+1][j]= Math.max(dp[i+1][j], tri[i+1][j] + dp[i][j]);
+            dp[i+1][j+1]= Math.max(dp[i+1][j+1], tri[i+1][j+1] + dp[i][j]);
             }
         }
         
-        
-        //4. 마지막 줄을 돌면서 가장 큰 값을 찾음
-        for(int x: dp[n-1])
-            ans = Math.max(ans,x);
-        
-        
+        for(int val: dp[n-1])
+            ans= Math.max(val, ans);
         
         return ans;
     }
+    
 }
