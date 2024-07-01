a5e1a48e8131e9da6ab40cd02d4a74211dd49b49
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-02 01:00:08 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int ans = 0;
+        while(a != b){
+            
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            
+            ans++;
+        }
+
+        return ans;
+    }
+}
