fdda27b00de4ee28a8e0f1fa88c28f7c30f3f441
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-05 23:08:05 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,16 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int ans = 1;
+        int n= clothes.length;
+        Map<String, Integer> map= new HashMap<>();
         
-        Map<String, Integer> map= new HashMap();
+        for(int i=0; i<n; i++)
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) +1);
         
-        for(String[] cloth: clothes)
-            map.put(cloth[1], map.getOrDefault(cloth[1], 1) +1);
-            
         for(String key: map.keySet()){
-            ans *=  map.get(key);
+              ans *= map.get(key)+1;
         }
-            
+          
+        
         return ans -1;
+        
     }
 }
