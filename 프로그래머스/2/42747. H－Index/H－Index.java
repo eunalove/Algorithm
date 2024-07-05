9db66dab14e8cf2f79f9cb0d98284bfc7fe5024e
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-06 00:03:38 +0900

    [level 2] Title: H-Index, Time: 0.33 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,32 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int ans = 0;
         
-        //알고리즘: DFS/BFS/DP/냅색/브루트포스/MST/다이나믹/그래프/유니온파인드/정렬/이분탐색
-        
-        //정렬-> 브루트포스로 하나하나 돌리기
-        
-        Arrays.sort(citations); //10, 11
+        Arrays.sort(citations);
         int n= citations.length;
-        
-        for(int h=1; h<=n; h++){ //2
-            
-        int citation= citations[h-1]; //11
-        int h2= 0;
-            
-            for(int j=0; j<n; j++){ //0
-                
-                if(citations[j] >= h) h2++;
-            }
-            
-            
-            //h2번 이상 인용된 논문이 h편 이상일 때
-            if(h2 >= h)
-            ans= h;
-            
+        int max= citations[n -1];
+        int idx= 0;
+        if(max == 0) return 0;
+        for(int i=0; i<=max; i++){
+            if(i >= citations[idx]) idx++;
+            if(i >= n -idx) return i;
         }
+            
+        return -1;
         
-        return ans;
     }
 }
