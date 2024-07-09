8b2b58920cc05adbe76037fe464544d563a224ec
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-09 17:25:10 +0900

    [level 2] Title: 전화번호 목록, Time: 207.01 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phoneBook) {
-        Map<String, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < phoneBook.length; i++) 
-            map.put(phoneBook[i], i);
+        Set<String> set= new HashSet<>();        
         
-        for (int i = 0; i < phoneBook.length; i++)
-            for (int j = 0; j < phoneBook[i].length(); j++)
-                if (map.containsKey(phoneBook[i].substring(0, j)))
-                    return false;
-
+        for(String book: phoneBook)
+            set.add(book);
+        
+        for(String book: phoneBook){
+            for(int i=0; i<book.length() -1; i++){
+                
+                String st= book.substring(0, i+1);
+                if(set.contains(st)) return false;
+            }
+        }
+        
         return true;
     }
 }
