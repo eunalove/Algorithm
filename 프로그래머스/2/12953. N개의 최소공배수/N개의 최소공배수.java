d18c9554a6914a8279f021ffd3af2433f02c2e92
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-10 11:36:08 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] arr) {
+        
+        int val= arr[0];
+        for(int i=1; i<arr.length; i++)
+            val= val * arr[i] / gcd(val, arr[i]);
+        
+        return val;
+    }
+    
+    static int gcd(int a, int b){
+        
+        int tmp;
+        
+        while(b != 0){
+            
+            tmp= a%b;
+            a= b;
+            b= tmp;
+        }
+        return a;
+        
+    }
+}
