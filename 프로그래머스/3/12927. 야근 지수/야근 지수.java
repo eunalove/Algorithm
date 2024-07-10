205cf4d76d850d841956fd4c5becbf61e903809e
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-11 01:02:14 +0900

    [level 3] Title: 야근 지수, Time: 117.10 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,28 +2,24 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        long ans = 0;
         
-        //최대값을 계속 유지하기 위해 pq생성
-        PriorityQueue<Integer> pq= new PriorityQueue<Integer>(Collections.reverseOrder());
-        int len= works.length;
+        PriorityQueue<Integer> pq= new PriorityQueue<>((pq1, pq2)-> pq2- pq1);
         
-        for(int i=0; i<len; i++)
-            pq.add(works[i]);
+        for(int work: works)
+            pq.add(work);
         
-        //n만큼 돌면서 works에서 가장 큰 값에서 1씩 뺀다
-        for(int i=0; i<n; i++){
-            int tmp= pq.poll();
-            if(tmp > 0) //만약 마이너스라면 마이너스*마이너스로 예상치 못한 값이 나올 수 있음
-            pq.offer(tmp-1);
-            else pq.offer(0);
+        while(!pq.isEmpty() && n>0){
+            if(pq.peek() >= 1)
+            pq.add(pq.poll() -1);
+            
+            n--;
         }
+        long ans= 0;
         
-        while(!pq.isEmpty()){//남은 일의 작업량으로 야근지수를 구한다
-            int tmp= pq.poll();
-            ans+= tmp*tmp;
-        }
+        for(int tmp: pq)
+            ans+=  tmp*tmp;
         
-        return ans;
+        if(!pq.isEmpty()) return ans;
+        else return 0;
     }
 }
