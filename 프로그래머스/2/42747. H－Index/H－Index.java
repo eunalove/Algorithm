19a99d81d15aa9c2430b9dafda9732aba7102d55
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-10 20:19:15 +0900

    [level 2] Title: H-Index, Time: 0.34 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        
         Arrays.sort(citations);
+        int ans= 0;
         int n= citations.length;
-        int max= citations[n -1];
-        int idx= 0;
-        if(max == 0) return 0;
-        for(int i=0; i<=max; i++){
-            if(i >= citations[idx]) idx++;
-            if(i >= n -idx) return i;
-        }
-            
-        return -1;
         
+        for(int h=1; h<n+1; h++){
+            for(int i=0; i<n; i++)
+                if(citations[i] >= h){
+                    if(n - i >= h){
+                        ans= Math.max(ans, h);
+                        break;
+                    }
+                }
+        }
+        
+        return ans;
     }
 }
