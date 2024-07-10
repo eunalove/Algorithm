e55a093f86afd1fdc6d58c4c509a079cc5d73e3c
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-11 00:32:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.20 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,46 +1,37 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx= {-1, 1, 0, 0};
-    static int[] dy= {0, 0, -1, 1};
-    
-    static class Node{
-        int x; int y; int time;
-        public Node(int x, int y, int time){
-            this.x= x;
-            this.y= y;
-            this.time= time;
-        }
-    }
     
     public int solution(int[][] maps) {
-        int ans = 0;
+        
+        int[] dx= {-1, 1, 0, 0};
+        int[] dy= {0, 0, -1, 1};
+        
         int n= maps.length;
         int m= maps[0].length;
         
-        Queue<Node> q= new ArrayDeque<Node>();
-        q.add(new Node(0,0,0));
-        
+        Queue<int[]> q= new ArrayDeque<>();
         boolean[][] vis= new boolean[n][m];
-        vis[0][0]= true;
+        q.add(new int[]{0, 0, 1});
         
         while(!q.isEmpty()){
             
-            Node node= q.poll();
-            if(node.x == n-1 && node.y == m-1) return node.time+1;
+            int[] cur= q.poll();
+            
+            if(cur[0]== n-1 && cur[1]== m-1) return cur[2];
             
             for(int i=0; i<4; i++){
-                int nx= node.x+ dx[i];
-                int ny= node.y+ dy[i];
                 
-                if(nx<0 || ny<0 || nx>=n || ny>=m || maps[nx][ny]==0 ) continue;
+                int nx= cur[0] + dx[i];
+                int ny= cur[1] + dy[i];
+                
+                if(nx<0 || ny<0 || nx>=n || ny>=m|| maps[nx][ny] == 0) continue;
                 
                 if(!vis[nx][ny]){
                     vis[nx][ny]= true;
-                    q.add(new Node(nx, ny, node.time+1));
+                    q.add(new int[]{nx, ny, cur[2]+1});
                 }
             }
-            
         }
         
         return -1;
