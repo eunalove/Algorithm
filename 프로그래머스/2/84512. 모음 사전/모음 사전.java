29c904ac09a44f3b740dfe277872cc67a73e1382
eunalove <88129325+eunalove@users.noreply.github.com>
2024-07-11 23:48:58 +0900

    [level 2] Title: 모음 사전, Time: 17.79 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 class Solution {
     static char[] words= {'A','E','I','O','U'};
-    static int cnt, ans;
+    static int cnt, ans= -1;
     
     public int solution(String word) {
         
         dfs(0, "", word);
-        return ans-1;
+        return ans -1;
     }
     
     static void dfs(int L, String cur, String word){
-        
+        if(ans>0) return;
         if(L>5) return;
         cnt++;
         
