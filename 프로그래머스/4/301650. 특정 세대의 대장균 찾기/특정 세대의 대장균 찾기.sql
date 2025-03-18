10995d46b4fb216903bae0d2ef8abf95d3043973
eunalove <88129325+eunalove@users.noreply.github.com>
2025-03-19 01:37:56 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -0,0 +1,9 @@
+select ID
+from ECOLI_DATA
+where PARENT_ID in ( select ID
+                    from ECOLI_DATA
+                        where PARENT_ID in (select ID
+                        from ECOLI_DATA
+                        where PARENT_ID is null)
+                    )
+order by ID
